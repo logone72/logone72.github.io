344ec463ab8f1467e8d027b9fafdccf0d5915499
logan <logan@teamo2.kr>
2023-03-13 17:38:15 +0900

    remove dist and update setting

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,20 @@
 export default {
     title: 'logone72 blog',
     description: 'frontend developer logone72 blog',
-    base: '/logone72.github.io/'
+    // base: '',
+
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Blog', link: '/노트/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            {
+                text: "머리", items: [
+                    { text: "🏛 노트", link: "/노트/" },
+                ],
+            },
+        ]
+    },
 }
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,18 @@
+import DefaultTheme from 'vitepress/theme'
+// import Layout from './Layout.vue'
+// import ImageWithCaption from '../components/ImageWithCaption.vue'
+// import BookAndProgress from '../components/BookAndProgress.vue'
+// import ProgressBar from '../components/ProgressBar.vue'
+// import StreakOnDay from '../components/StreakOnDay.vue'
+// import './custom.scss'
+
+export default {
+    ...DefaultTheme,
+    // Layout,
+    // enhanceApp(ctx) {    // extend default theme custom behaviour.
+    //     ctx.app.component('ImageWithCaption', ImageWithCaption)
+    //     ctx.app.component('BookAndProgress', BookAndProgress)
+    //     ctx.app.component('ProgressBar', ProgressBar)
+    //     ctx.app.component('StreakOnDay', StreakOnDay)
+    // }
+}
