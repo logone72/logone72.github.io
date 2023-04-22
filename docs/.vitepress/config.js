fde1c2d20711e6b4f2ae306f42710510aff2b5b8
logan <logan@teamo2.kr>
2023-04-22 21:14:19 +0900

    feat: SEO - google site verification

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,9 @@ import sidebarConfig from './sidebarConfig.json'
 export default {
     title: 'logone72 blog',
     description: 'frontend developer logone72 blog',
+    head: [
+      ['meta', {name: 'google-site-verification', content: 'f5b8urfLVM-IRsORykWqUAMVws-ISJnIYUiFhZDF1rU'}]
+    ],
 
     themeConfig: {
         nav: [
