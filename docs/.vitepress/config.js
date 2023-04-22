d12d52a19c7805cae8997ac859d874037801a16c
Roegan Kim / 김록원 <roegan.kim@gmail.com>
2023-04-22 20:44:48 +0900

    feat: obsidian Converter ver 1
    * clean up test files
    
    * add: config.js, constraint.js
    
    * install: fs-extra
    
    * feat: add util/fileConverter.js
    
    * feat: update .gitignore
    
    * feat: add file converter manage : named Lavencha
    
    * feat: add test script
    
    * feat: add convert script
    
    * feat: update .gitignore
    
    * feat: add sidebar config creator
    
    * feat: add sidebarConfig.json and add script for sidebar
    
    * fix: 일부 string이 빌드 오류 일으키는 현상 수정
    
    ---------
    
    Co-authored-by: logan <logan@teamo2.kr>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,6 +9,6 @@ export default {
             {text: 'Home', link: '/'},
             {text: 'Github', link: 'https://github.com/logone72/logone72.github.io'},
         ],
-        sidebar: sidebarConfig
+        sidebar: sidebarConfig,
     },
 }
