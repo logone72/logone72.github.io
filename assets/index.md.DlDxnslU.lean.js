e94f851744243bb654ef47a72fd0d046eb26d5a1
logone72 <logone72@users.noreply.github.com>
2024-09-08 11:18:58 +0000

    deploy: 340ad7ba503e4aa1c06623a92ca5ed870f88041a

---
+++ assets/index.md.DlDxnslU.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as r,o as t,a3 as a}from"./chunks/framework.BgY3BpbC.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),i={name:"index.md"},o=a("",32),s=[o];function l(n,h,p,d,b,g){return t(),r("div",null,s)}const u=e(i,[["render",l]]);export{f as __pageData,u as default};
