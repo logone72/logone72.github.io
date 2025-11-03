42edcd8c1cdba560ab2342c010d0f06786615c04
Roegan Kim 김록원 <roegan.kim@gmail.com>
2025-11-03 17:28:35 +0900

    chore: clarity 추적 코드 삽입

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -34,6 +34,16 @@ export default defineConfig(
             content: '67ff08576a160f454495df90f7f4021201a0b4da',
           },
         ],
+        [
+          'script',
+          { type: 'text/javascript' },
+          `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "u08gul678r");
+          `,
+        ],
         ['link', { rel: 'icon', href: '/favicon.ico' }],
       ],
       lang: 'ko-KR',
